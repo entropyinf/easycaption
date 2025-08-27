6f0b03e196d4e9173c60e4bf7aea1b0d755e98fc
entropy <cglibproxy@163.com>
2025-08-27 11:00:23 +0800

    chore: remove whisperlivekit

--- src/pages/caption.tsx
+++ src/pages/caption.tsx
@@ -20,8 +20,8 @@ export default function Caption() {
     }, [])
 
     return (
-        <div className="h-full bg-black/50 text-white p-4 rounded-md flex flex-col absolute bottom-0 left-0 right-0 select-none" >
-            <h1 className="text-3xl font-medium text-center select-none" data-tauri-drag-region>
+        <div className="h-full bg-black/50 text-white p-4 rounded-md flex flex-col absolute bottom-0 left-0 right-0 select-none" data-tauri-drag-region>
+            <h1 className="text-3xl font-medium text-center select-none" >
                 {caption}
             </h1>
         </div>
